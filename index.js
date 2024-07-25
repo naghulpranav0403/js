bc2eff17dd391f796658d6477362585796c658a2
naghulpranav0403 <ntaghulpranav@gmail.com>
2024-07-25 12:06:19 +0530

    commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// const greet = (name) => {
+//     return' hello,${name}!!';
+// };
+// console.log(greet("NAGHUl"));
+// const obj = {
+//     name: "Sri krishna college of technology",
+//     reguralFunction: function(){
+//         console.log(this.name + " naghul Pranav");
+//     },
+//     arrowFunction: (suffix) => {
+//         console.log(obj.name + " " + suffix);
+//     }
+// };
+
+// obj.reguralFunction();
+// obj.arrowFunction("sai rishi");
+// const numbers =[1,2,3];
+// const morenumbers =[4,5,6];
+// const allNum =[...numbers,...morenumbers];
+// console.log(allNum)
+// const extended = [0,...numbers,...morenumbers];
+// console.log(extended);
+// const numbers =[1,2,3];
+// const maxNumber = Math.min(...numbers);
+// console.log(maxNumber);
+// const person ={name: "Naghul",age:19};
+
+// const updatedperson ={...person,age:20};
+// console.log(updatedperson);
+// const updatedperson1 ={...person,city:"coimbatore"};
+// console.log(updatedperson1);
+// const college={
+//     name:"naghul",
+//     age:"23",
+//     rollno:"727822TUEC128",
+//     city:"Coimbatore",
+// };
+// console.log(college);
+// const numbers =[1,2,3,4,5,6];
+// const evenNumbers = numbers.filter(num=>num>2);
+
+// console.log(evenNumbers);
+const numbers =[1,2,3,4,5,6];
+
+// const squarednumber = numbers.map(num=>num*num);
+// console.log(numbers.filter(num=>num%4==1));
+// console.log(squarednumber);
+const sum = numbers.reduce((acc,val)=> acc+val,0);
+console.log(sum);
